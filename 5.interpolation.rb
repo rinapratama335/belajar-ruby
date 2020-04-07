dd505395f4f995715231cc8f213bb46454a82849
irwantowibowo1992 <irwantowibowo1992@hotmail.com>
2020-04-07 15:05:02 +0700

    string interpolation

---
+++ 5.interpolation.rb
@@ -0,0 +1,8 @@
+#Untuk menggunakan string interpolation maka kita harus menggunakan tanda peti dua (" "), jika pakai tanda petik satu maka hanya dianggap string biasa
+
+nama = 'Irwanto'
+alamat = 'Gunungkidul'
+
+puts "nama saya #{nama} dana alamat saya di #{alamat}"
+
+puts "Hasil = #{10 * 2}"

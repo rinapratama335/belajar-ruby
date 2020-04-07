9349f6fe555a5fe64ca34128cb49ab13807494f2
irwantowibowo1992 <irwantowibowo1992@hotmail.com>
2020-04-07 14:33:08 +0700

    variabel pada ruby

---
+++ 1.variabel.rb
@@ -0,0 +1,24 @@
+mahasiswa = "Irwanto Wibowo"
+nilai = 95
+
+# menampilkan nilai variabel
+puts mahasiswa
+puts nilai
+
+puts
+puts
+
+angkaSatu = 40
+angkaDua = 5
+
+hasilTambah = angkaSatu + angkaDua
+hasilKurang = angkaSatu - angkaDua
+hasilKali = angkaSatu * angkaDua
+hasilBagi = angkaSatu / angkaDua
+hasilModulus = angkaSatu % angkaDua
+
+puts "Hasil penambahan #{hasilTambah}"
+puts "Hasil pengurang #{hasilKurang}"
+puts "Hasil perkalian #{hasilKali}"
+puts "Hasil pembagian #{hasilBagi}"
+puts "Hasil modulus #{hasilModulus}"

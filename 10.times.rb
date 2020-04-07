4b7aa586a8a34f6df8ad13cd842b98a537a29fd0
irwantowibowo1992 <irwantowibowo1992@hotmail.com>
2020-04-07 17:43:32 +0700

    melakukan perulangan dengan fungsi times

---
+++ 10.times.rb
@@ -0,0 +1,15 @@
+#Fungsi times adalah salah satu fungsi yang bisa kita gunakan untuk melakukan perulangan
+
+5.times do
+    puts "Haloo ruby yang keren"
+end
+
+puts puts
+
+3.times { puts "Assalamualaikum" }
+
+puts puts
+
+5.times do |nomor|
+    puts "Haloo ruby yang keren #{nomor + 1}"
+end 

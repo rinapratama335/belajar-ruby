16fbed7df3e32c16903dc3b253af6374a7a0a758
irwantowibowo1992 <irwantowibowo1992@hotmail.com>
2020-04-07 18:07:42 +0700

    belajar ruby

---
+++ 2.Boolean dan Logika/1.operator_perbandingan.rb
@@ -0,0 +1,32 @@
+puts 80 > 90
+puts 80 < 90
+puts 90 >= 91
+puts 90 >= 90
+
+puts 90 == 91
+puts
+
+puts 'irwanto' == 'irwanto'
+puts 'password' == 'password123'
+
+puts
+puts
+
+puts true && false
+puts true && true
+puts false && true
+puts false && false
+
+puts puts
+
+puts true || false
+puts false || false
+puts false || true
+puts true || true
+
+puts puts
+
+puts !true
+puts !false
+puts !!true
+puts !!false
